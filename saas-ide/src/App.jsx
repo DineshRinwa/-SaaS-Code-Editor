f67c1f6f70d46632bd2e957727513afe4f828c2e
DineshRinwa <dineshrinwa410@gmail.com>
2025-03-12 20:55:35 +0530

    Change the error heading

--- saas-ide/src/App.jsx
+++ saas-ide/src/App.jsx
@@ -4,9 +4,8 @@ import { AllRoute } from "./Component/AllRoutes";
 function App() {
   return (
     <div>
-      <p className="bg-gradient-to-r from-blue-500 to-purple-600 text-white font-bold font-sans text-md text-center tracking-wider py-2 rounded-lg shadow-md">
-        For the best experience, please use this app in the Chrome browser!
-        (Clerk)
+      <p className="bg-gradient-to-r from-blue-300 to-purple-500 text-black font-bold text-md text-center tracking-wider py-2 rounded-lg shadow-md font-mono">
+      If the server sleeps, refresh 2-3 times!
       </p>
 
       <AllRoute />
